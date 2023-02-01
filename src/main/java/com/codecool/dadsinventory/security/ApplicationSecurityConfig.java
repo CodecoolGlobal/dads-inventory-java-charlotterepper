ec76b9ee5ddedba3c014876d6dcd9f4139fe84ff
charlotterepper <charlotte.repper01@gmail.com>
2023-02-01 20:02:57 +0100

    Add password encoding and users

--- src/main/java/com/codecool/dadsinventory/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codecool/dadsinventory/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.codecool.dadsinventory.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +33,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails dad = User.builder()
+                .username("dad")
+                .password(passwordEncoder.encode("password"))
+                .build();
+
+        UserDetails mom = User.builder()
+                .username("mom")
+                .password(passwordEncoder.encode("password123"))
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                dad,
+                mom
+        );
     }
 }
