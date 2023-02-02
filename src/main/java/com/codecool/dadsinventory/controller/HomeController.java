114a88b4499cd0d41461d6f8be08ef943080008b
charlotterepper <charlotte.repper01@gmail.com>
2023-02-02 17:18:48 +0100

    Display logged-in username

--- src/main/java/com/codecool/dadsinventory/controller/HomeController.java
+++ src/main/java/com/codecool/dadsinventory/controller/HomeController.java
@@ -7,12 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @Controller
@@ -34,9 +36,14 @@ public class HomeController {
         List<Item> items = itemService.getAllBySearchTerm(searchTerm);
         model.addAttribute("title", title);
         model.addAttribute("items", items);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(auth.getPrincipal());
-        model.addAttribute("principal", auth.getPrincipal());
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        model.addAttribute("principal", username);
         return "index";
     }
 
